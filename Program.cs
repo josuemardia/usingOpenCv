4cbc7a7ec3b10ba5ed58bfb55d32355bf79a827c
Josue MarDi <originaljosue@gmail.com>
2023-11-29 20:13:02 -0500

    terminando

--- Program.cs
+++ Program.cs
@@ -2,6 +2,11 @@
 //Console.WriteLine("Hello, World!");
 using System;
 using System.IO;
+// using System.Drawing;
+// using Emgu.CV;
+// using Emgu.CV.CvEnum;
+// using Emgu.CV.Structure;
+//using Emgu.CV.Util;
 using Microsoft.VisualBasic;
 using OpenCvSharp;
 
@@ -21,16 +26,15 @@ namespace TestDigitalSolution324
                     return;
                 }
 
-                string outputFolder = "video/salida";// Crea una carpeta para guardar las imágenes
+                string outputFolder = "frames/captures";// Crea una carpeta para guardar las imágenes
                 Directory.CreateDirectory(outputFolder);
 
-                // Lee el video y guarda cada frame como una imagen
-                Mat frame = new Mat();
+                Mat frame = new Mat(); // guarda cada frame como una imagen
                 int frameCount = 0;
                 while (true)
                 {
                     
-                    if (!video.Read(frame))// Lee el siguiente frame
+                    if (!video.Read(frame))
                     {
                         break;
                     }
@@ -39,59 +43,54 @@ namespace TestDigitalSolution324
                     string imagePath = Path.Combine(outputFolder, $"frame{frameCount:D5}.jpg");
                     Cv2.ImWrite(imagePath, frame);
 
-                    frameCount++; // Incrementador
+                    frameCount++;
                 }
 
                 Console.WriteLine($"Se guardaron {frameCount} imágenes en la carpeta {outputFolder}.");
             }
         }
 
-        public static void AnalyzeFace(){
-            // Ruta del archivo de video
+        public static void AnalyzeFace(){ //Solo detecta los rostros
+
             string videoPath = "video/analyzePeople.mp4";
 
-            // Crea un objeto VideoCapture para leer el video
             using (var video = new VideoCapture(videoPath))
             {
-                // Verifica si el video se abrió correctamente
-                if (!video.IsOpened())
+                
+                if (!video.IsOpened()) // Validacion si el video se abrió correctamente
                 {
                     Console.WriteLine("No se pudo abrir el archivo de video.");
                     return;
                 }
 
-                // Crea una carpeta para guardar las imágenes
-                string outputFolder = "video/analyzedFaces";
+                string outputFolder = "video/analyzedFaces"; // Crea una carpeta para guardar las imágenes
                 Directory.CreateDirectory(outputFolder);
 
-                // Crea un objeto CascadeClassifier para detectar caras
+                //Para detectar caras
                 CascadeClassifier faceDetector = new CascadeClassifier("data/haarcascade_frontalface_default.xml");
 
-                // Lee el video y analiza cada frame
+                //Analiza cada frame
                 Mat frame = new Mat();
                 int frameCount = 0;
                 while (true)
                 {
-                    // Lee el siguiente frame
+                   
                     if (!video.Read(frame))
                     {
                         break;
                     }
 
-                    // Detecta las caras en el frame
-                    Rect[] faces = faceDetector.DetectMultiScale(frame);
+                    Rect[] faces = faceDetector.DetectMultiScale(frame); //Para detectar las caras en el frame
 
-                    // Dibuja un rectángulo alrededor de cada cara detectada
-                    foreach (Rect face in faces)
+                    foreach (Rect face in faces) // Se encasilla cada cara que se detecta
                     {
                         Cv2.Rectangle(frame, face, Scalar.Red, 2);
                     }
 
-                    // Guarda el frame con los rectángulos dibujados
+                    //Guarda el frame con los rectángulos dibujados
                     string imagePath = Path.Combine(outputFolder, $"frame{frameCount:D5}.jpg");
                     Cv2.ImWrite(imagePath, frame);
 
-                    // Incrementa el contador de frames
                     frameCount++;
                 }
 
@@ -99,9 +98,85 @@ namespace TestDigitalSolution324
             } 
         }
     
+        public static void FacialDifferences(){
+            
+            VideoCapture capture = new VideoCapture("video/analyzePeople.mp4");
+
+            //Para detectar caras
+            CascadeClassifier faceCascade = new CascadeClassifier("data/haarcascade_frontalface_default.xml");
+
+            //Objeto de comparacion de histograma
+            HistCompMethods method = HistCompMethods.Correl;
+            double Limit = 0.5; //umbral
+            Mat[] histograms = new Mat[2];
+            histograms[0] = new Mat();
+            histograms[1] = new Mat();
+
+            
+            while (true) //Recorrido de los frames 
+            {
+               
+                Mat frame = new Mat();
+                capture.Read(frame);
+
+                if (frame.Empty())
+                {
+                    break;
+                }
+
+                //Convierte el frame a escala de grises siguiendo el estandar de deteccion de caras
+                Mat gray = new Mat();
+                Cv2.CvtColor(frame, gray, ColorConversionCodes.BGR2GRAY);
+
+                Rect[] faces = faceCascade.DetectMultiScale(gray, 1.3, 5);
+
+
+                foreach (Rect face in faces)  // Recorre el arreglo donde estan todas las caras detectadas  
+                {
+                    // Extract the face from the frame
+                    Mat faceMat = new Mat(frame, face);
+
+                    //Calcula el histograma de la cara
+                    Cv2.CalcHist(new Mat[] { faceMat }, new int[] { 0 }, new Mat(), histograms[0], 1, new int[] { 256 }, new Rangef[] { new Rangef(0, 256) });
+                    Cv2.Normalize(histograms[0], histograms[0], 0, 1, NormTypes.MinMax);
+
+                    // Loop through the histograms of the other faces
+                    DirectoryInfo dir = new DirectoryInfo("faces");
+                    FileInfo[] files = dir.GetFiles("*.jpg");
+                    foreach (FileInfo file in files)
+                    {
+                        Mat otherHist = new Mat();
+                        /* //Cv2.ImRead(file.FullName, ImreadModes.GrayScale).ConvertTo(otherHist, MatType.CV_32F);*/
+                        Cv2.CalcHist(new Mat[] { otherHist }, new int[] { 0 }, new Mat(), histograms[1], 1, new int[] { 256 }, new Rangef[] { new Rangef(0, 256) });
+                        Cv2.Normalize(histograms[1], histograms[1], 0, 1, NormTypes.MinMax);
+
+                        //Compara los histogramas
+                        double result = Cv2.CompareHist(histograms[0], histograms[1], method);
+
+                        // Si está por encima del umbral, las caras coinciden
+                        if (result > Limit)
+                        {
+                            // Para cada persona se guarda en su carpeta
+                            string name = Path.GetFileNameWithoutExtension(file.Name);
+                            string folder = Path.Combine("output", name);
+                            Directory.CreateDirectory(folder);
+                            string filename = Path.Combine(folder, Guid.NewGuid().ToString() + ".jpg");
+                            Cv2.ImWrite(filename, faceMat);
+                        }
+                    }
+
+                    //Guarda el histograma de la cara
+                    string histFilename = Path.Combine("faces", Guid.NewGuid().ToString() + ".jpg");
+                    Cv2.ImWrite(histFilename, faceMat);
+                }
+            }
+            Console.WriteLine($"proceso terminado!");
+        }
+
         public static void Main(string[]args){
             CaptureFrames();
-            AnalyzeFace();
+            //AnalyzeFace();
+            FacialDifferences();
         }
     }
         
